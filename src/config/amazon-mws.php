d36fe7b4202ef2adff1337922a90e4c17300bc46
Tim Ramsey <timramseyjr@users.noreply.github.com>
2018-06-05 13:38:01 -0400

    Updated default config to use env values

--- src/config/amazon-mws.php
+++ src/config/amazon-mws.php
@@ -2,17 +2,15 @@
 
 return [
 	'store' => [
-		'store1' => [
-			'merchantId' => '',
-			'marketplaceId' => '',
-			'keyId' => '',
-			'secretKey' => '',
-			'amazonServiceUrl' => 'https://mws-eu.amazonservices.com/',
+		'store' => [
+			'merchantId' => env('AMAZON_SELLER_ID',''),
+			'marketplaceId' => env('AMAZON_MARKETPLACE_ID',''),
+			'keyId' => env('AMAZON_AWS_ACCESS_KEY_ID',''),
+			'secretKey' => env('AMAZON_AWS_SECRET_ACCESS_KEY',''),
+			'amazonServiceUrl' => env('AMAZON_SERVICE_URL','https://mws.amazonservices.com/'),
+			'MWSAuthToken' => env('AMAZON_MWS_AUTH_TOKEN','')
 		]
 	],
-
-	// Default service URL
-	'AMAZON_SERVICE_URL' => 'https://mws.amazonservices.com/',
-
-	'muteLog' => false
+	'AMAZON_SERVICE_URL' => env('AMAZON_SERVICE_URL','https://mws.amazonservices.com/'),
+	'muteLog' => env('AMAZON_MUTE_LOG',false)
 ];
